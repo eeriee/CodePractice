7ef40c3a0ba4f151745352c53faf3f3eba6aaa20
Xi <gxtogether@gmail.com>
2016-07-01 17:38:09 +0800

    Create Inorder Successor in BST_285.cpp

---
+++ Tree/Inorder Successor in BST_285.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if(p->right != NULL){
+            return minVal(p->right);
+        }
+        
+        TreeNode* succ = NULL, *node = root;
+        while(node != NULL){
+            if(node->val < p->val){
+                node = node->right;
+            }else if(node->val > p->val){
+                succ = node;
+                node = node->left;
+            }else break;
+        }
+        return succ;
+    }
+    
+    TreeNode* minVal(TreeNode* node){
+        TreeNode* p = node;
+        while(p->left != NULL){
+            p = p->left;
+        }
+        return p;
+    }
+};
