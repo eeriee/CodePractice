b16f77da51c4c7a58c032e87a57add68877ea40b
eeriee <gxtogether@gmail.com>
2016-10-02 21:34:48 -0400

    loop queue, each time it only increase the neighbor+1

--- Breadth_First_Search/Walls and Gates_286.cpp
+++ Breadth_First_Search/Walls and Gates_286.cpp
@@ -22,38 +22,32 @@ public:
             int cur = rooms[vi][vj];
             if(vi > 0){
                 int* up = &rooms[vi-1][vj];
-                if(update(cur, *up)){
+                if(*up = INT_MAX){
                     *up = cur+1;
                     q.push(make_pair(vi-1, vj));
                 }
             }
             if(vi+1<ro){
                 int* down = &rooms[vi+1][vj];
-                if(update(cur, *down)){
+                if(*down = INT_MAX){
                     *down = cur+1;
                     q.push(make_pair(vi+1, vj));
                 }
             }
             if(vj > 0){
                 int* left = &rooms[vi][vj-1];
-                if(update(cur, *left)){
+                if(*left = INT_MAX){
                     *left = cur+1;
                     q.push(make_pair(vi, vj-1));
                 }
             }
             if(vj+1<co){
                 int* right = &rooms[vi][vj+1];
-                if(update(cur, *right)){
+                if(*right = INT_MAX){
                     *right = cur+1;
                     q.push(make_pair(vi, vj+1));
                 }
             }
         }
-    }
-    bool update(int v, int u){
-        if( u == INT_MAX && u > v+1){
-            return true;
-        }
-        return false;
     }
 };
