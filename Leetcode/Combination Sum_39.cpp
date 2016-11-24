b0f2d18ff79adf276afe2b8dd13f5da04b92df1e
Xi Wang <gxtogether@gmail.com>
2016-11-23 22:35:54 -0500

    remember the state

---
+++ Leetcode/Combination Sum_39.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end(), greater<int>());
+        vector<vector<int>> res;
+        vector<int> ve;
+        helper(0, target, res, ve, candidates);
+        return res;
+    }
+    void helper(int pos, int target, vector<vector<int>> &res, vector<int> &ve, vector<int>& cands){
+        for(int i = pos; i < cands.size(); ++i){
+            int cand = cands[i];
+            if(cand > target) continue;
+            ve.push_back(cand);
+            int ans = target - cand;
+            if(ans == 0){
+                res.push_back(ve);
+            }else{
+                helper(i, ans, res, ve, cands);
+            }
+            ve.pop_back();
+        }
+    }
+};
