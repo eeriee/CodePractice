a2fbccee2e604dbb1ad954d0ad844ee4b92d8f18
Xi Wang <gxtogether@gmail.com>
2017-01-11 17:17:47 -0500

    update. it's a really good recursion with updated index

--- Leetcode/Decode String_394.cpp
+++ Leetcode/Decode String_394.cpp
@@ -1,3 +1,35 @@
+//dfs
+class Solution {
+public:
+    string decodeString(string s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        
+        return res;
+    }
+
+    string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
+    }
+};
+// stupid way - stack
 class Solution {
 public:
     string decodeString(string s) {
@@ -39,36 +71,5 @@ public:
         }
         reverse(res.begin(), res.end());
         return res;
-    }
-};
-
-class Solution {
-public:
-    string decodeString(string s, int& i) {
-        string res;
-        
-        while (i < s.length() && s[i] != ']') {
-            if (!isdigit(s[i]))
-                res += s[i++];
-            else {
-                int n = 0;
-                while (i < s.length() && isdigit(s[i]))
-                    n = n * 10 + s[i++] - '0';
-                    
-                i++; // '['
-                string t = decodeString(s, i);
-                i++; // ']'
-                
-                while (n-- > 0)
-                    res += t;
-            }
-        }
-        
-        return res;
-    }
-
-    string decodeString(string s) {
-        int i = 0;
-        return decodeString(s, i);
     }
 };
