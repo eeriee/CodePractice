14e7253236eaebbf63abb52cef4c6ed10c98e795
Xi Wang <gxtogether@gmail.com>
2017-01-11 16:03:29 -0500

    update, easy

--- Leetcode/Binary Tree Longest Consecutive Sequence_298.cpp
+++ Leetcode/Binary Tree Longest Consecutive Sequence_298.cpp
@@ -7,6 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// new one
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+        if(root == NULL) return 0;
+        int maxLen = 1;
+        helper(root, root->val, maxLen, 0);
+        return maxLen;
+    }
+    void helper(TreeNode* node, int prev, int& maxLen, int curLen){
+        if(node == NULL) return;
+        int val = node->val;
+        if(prev == val - 1){
+            maxLen = max(maxLen, curLen+1);
+        }else{
+            curLen = 0;
+        }
+        ++curLen;
+        helper(node->left, val, maxLen, curLen);
+        helper(node->right, val, maxLen, curLen);
+    }
+};
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+        if(root == NULL) return 0;
+        dfs(root, 0, root->val);
+        return maxLen;
+    }
+    void dfs(TreeNode* node, int len, int val){
+        if(node == NULL) return;
+        if (node->val == val) ++len;
+        else len = 1;
+        maxLen = max(len, maxLen);
+        dfs(node->left, len, node->val+1);
+        dfs(node->right, len, node->val+1);
+    }
+    int maxLen = 0;
+};
+//old one
 class Solution {
 public:
     int longestConsecutive(TreeNode* root) {
@@ -40,22 +80,4 @@ public:
     }
     int maxLen;
 };
-/*
-class Solution {
-public:
-    int longestConsecutive(TreeNode* root) {
-        if(root == NULL) return 0;
-        dfs(root, 0, root->val);
-        return maxLen;
-    }
-    void dfs(TreeNode* node, int len, int val){
-        if(node == NULL) return;
-        if (node->val == val) ++len;
-        else len = 1;
-        maxLen = max(len, maxLen);
-        dfs(node->left, len, node->val+1);
-        dfs(node->right, len, node->val+1);
-    }
-    int maxLen = 0;
-};
-*/
+
