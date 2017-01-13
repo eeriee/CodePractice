c61506863cde76de896b3362f920e3be89163775
Xi Wang <gxtogether@gmail.com>
2017-01-12 22:03:31 -0500

    the question is really tricky, a greedy method

---
+++ Greedy/Wiggle Sort_280.cpp
@@ -0,0 +1,34 @@
+/*
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+*/
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); ++i){
+            if(( i%2 == 0 && nums[i] > nums[i-1])|| (i%2 == 1 && nums[i] < nums[i-1])){
+                swap(nums[i], nums[i-1]);
+            }
+        }
+    }
+};
+/*
+slow solution: remember the sort method.
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> tmp = nums;
+        for(int i = 0, j = 0, k = nums.size() - 1; i < nums.size(); ++i){
+            if(i % 2){
+                nums[i] = tmp[k];
+                --k;
+            }else{
+                nums[i] = tmp[j];
+                ++j;
+            }
+        }
+    }
+};
+*/
