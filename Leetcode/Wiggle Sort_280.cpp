6ac5f7047efb4c4e7c1eb660c1d69cbdd7257214
Xi Wang <gxtogether@gmail.com>
2017-01-12 22:03:14 -0500

    the question is really tricky, a greedy method

--- Leetcode/Wiggle Sort_280.cpp
+++ Leetcode/Wiggle Sort_280.cpp
@@ -8,9 +8,7 @@ public:
     void wiggleSort(vector<int>& nums) {
         for(int i = 1; i < nums.size(); ++i){
             if(( i%2 == 0 && nums[i] > nums[i-1])|| (i%2 == 1 && nums[i] < nums[i-1])){
-                int tmp = nums[i];
-                nums[i] = nums[i-1];
-                nums[i-1] = tmp;
+                swap(nums[i], nums[i-1]);
             }
         }
     }
