3986734c876f114f26c5278350f74453e8d551c2
Xi Wang <gxtogether@gmail.com>
2017-01-26 15:47:37 -0500

    tricky way. split sliding window into two windows

--- Leetcode/Sliding Window Maximum_239.cpp
+++ Leetcode/Sliding Window Maximum_239.cpp
@@ -1,3 +1,30 @@
+// O(n) fast way
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(nums.empty() || k <= 0) return {};
+        int n = nums.size();
+        vector<int> ltoRmax(n);
+        vector<int> rtoLmax(n);
+        for(int i = 0, j = n-1; i < n; ++i, --j){
+            if(i%k == 0){
+                ltoRmax[i] = nums[i];
+            }else{
+                ltoRmax[i] = max(ltoRmax[i-1], nums[i]);
+            }
+            if(j%k == k-1 || j == n-1){
+                rtoLmax[j] = nums[j];
+            }else{
+                rtoLmax[j] = max(rtoLmax[j+1], nums[j]);
+            }
+        }
+        vector<int> res;
+        for(int i = 0; i <= n-k; ++i){
+            res.push_back(max(rtoLmax[i], ltoRmax[i+k-1]));
+        }
+        return res;
+    }
+};
 // O(n)
 // https://abitofcs.blogspot.com/2014/11/data-structure-sliding-window-minimum.html
 class Solution {
