df6b0d039df3b69cc2364254a3edaf425fcc0899
Xi Wang <gxtogether@gmail.com>
2017-02-11 21:23:12 -0500

    in-order traversal for binary search tree

--- Leetcode/Binary Tree Inorder Traversal_94.cpp
+++ Leetcode/Binary Tree Inorder Traversal_94.cpp
@@ -1,3 +1,25 @@
+// iterator
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root == NULL) return true;
+        stack<TreeNode*> stk;
+        TreeNode* prev = NULL;
+        while(root != NULL || !stk.empty()){
+            while(root != NULL){
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if(prev != NULL && prev->val >= root->val) return false;
+            prev = root;
+            root = root->right;
+        }
+        return true;
+    }
+};
+// recursive
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
--- Leetcode/Validate Binary Search Tree_98.cpp
+++ Leetcode/Validate Binary Search Tree_98.cpp
@@ -7,6 +7,43 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//iterative - use inorder traversal
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root == NULL) return true;
+        stack<TreeNode*> stk;
+        TreeNode* prev = NULL;
+        while(root != NULL || !stk.empty()){
+            while(root != NULL){
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if(prev != NULL && prev->val >= root->val) return false;
+            prev = root;
+            root = root->right;
+        }
+        return true;
+    }
+};
+//recursive1 - no int_max and int_min
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        return validate(root, prev);
+    }
+    bool validate(TreeNode* node, TreeNode* &prev) {
+        if (node == NULL) return true;
+        if (!validate(node->left, prev)) return false;
+        if (prev != NULL && prev->val >= node->val) return false;
+        prev = node;
+        return validate(node->right, prev);
+    }
+};
+//recursive2
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
