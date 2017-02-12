fcea6414995cbba0bef8db65bb7a2c512241326f
Xi Wang <gxtogether@gmail.com>
2017-02-12 17:00:24 -0500

    dp

--- Leetcode/Unique Paths II_63.cpp
+++ Leetcode/Unique Paths II_63.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid.empty() || obstacleGrid[0].empty() || obstacleGrid[0][0]) return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<int> dp(n); 
+        dp[0] = 1; 
+        for(auto row: obstacleGrid){
+            for(int i = 0; i <n; ++i){
+                if(row[i]){
+                    dp[i] = 0;
+                }else if(i>0){
+                    dp[i] += dp[i-1]; 
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if(obstacleGrid.empty() || obstacleGrid[0].empty()) return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<vector<int>> dp(m+1, vector<int>(n+1,0)); 
