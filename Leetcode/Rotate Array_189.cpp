a18747547b5976f26680349ea45f951772eff87d
Xi Wang <gxtogether@gmail.com>
2017-02-13 20:00:30 -0500

    update

--- Leetcode/Rotate Array_189.cpp
+++ Leetcode/Rotate Array_189.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        int n = nums.size(), start = 0, idx = 0, curr = nums[0], pos = 0, cnt = 0;
+        while(cnt < n){
+            do{
+                pos = (idx+k)%n;
+                swap(curr, nums[pos]);
+                idx = pos;
+                ++cnt;
+            }while(idx != start);
+            ++start;
+            idx = start;
+            curr = nums[idx];
+        }
+    }
+};
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
         if(k > 0){
         reverse(nums.begin(), nums.end());
